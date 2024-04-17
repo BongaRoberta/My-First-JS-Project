a187410740a6923342e1d8bed21023fca95e591b
Bonga Roberta <113582608+BongaRoberta@users.noreply.github.com>
2024-04-18 01:59:54 +0200

    Update script.js

--- LoginPage/script.js
+++ LoginPage/script.js
@@ -1,6 +1,6 @@
 var container = document.getElementById('container');
-var registerBtn = document.getElementById('register');
-var loginBtn = document.getElementById('login');
+let registerBtn = document.getElementById('register');
+const loginBtn = document.getElementById('login');
 
 registerBtn.addEventListener('click',() =>{
     container.classList.add("active");
